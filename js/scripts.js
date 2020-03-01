7d9f7336a23d121f111eb716ea8dd4b792f3ad23
Manasseh Bundi <Manassehbundi2017@gmail.com>
2020-03-01 16:14:50 +0300

    Made toggle effect on the menu section so as to differentiate the different types of pizzas..either meat or veggies

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".clickable").click(function(){
+        $(".meat").toggle();
+    });
+    $(".veg").click(function(){
+        $(".veggie").toggle();
+    });
+    $(".cust").click(function(){
+        $(".custom").toggle();
+    });
+
+
+});
+
